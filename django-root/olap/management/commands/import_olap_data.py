2cf568cdab271c346d888001889a7a9e0a5e3a77
Mitch Davis <mitch@alliancesoftware.com.au>
2017-09-13 14:13:03 +1000

    feat: Mod importer to specify course offerings to import on cmdline

--- django-root/olap/management/commands/import_olap_data.py
+++ django-root/olap/management/commands/import_olap_data.py
@@ -1,10 +1,46 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
-from olap.lms_import import ImportLmsData
+from dashboard.models import CourseOffering
+
+from ...lms_import import ImportLmsData
 
 class Command(BaseCommand):
-    help = "Import OLAP data"
+    help = "Import LMS data for one or more course offerings"
+
+    def add_arguments(self, parser):
+        parser.add_argument('offering_code', nargs='+', type=str)
+
+        parser.add_argument('--clear',
+            action='store_true',
+            dest='clear',
+            default=False,
+            help='Clear data from db instead of importing it into db',
+        )
 
     def handle(self, *args, **options):
-        importer = ImportLmsData()
-        importer.process()
+        course_codes = options['offering_code']
+        verb = 'clear' if options['clear'] else 'import'
+
+        if course_codes == ['all']:
+            if len(CourseOffering.objects.all()) == 0:
+                raise CommandError('Cannot {} because there are no course offerings in the database.'.format(verb))
+            course_codes = tuple(course_offering.code for course_offering in CourseOffering.objects.all())
+        else:
+            co_errors = []
+            for course_code in course_codes:
+                if not CourseOffering.objects.filter(code=course_code).exists():
+                    co_errors.append('There is no course offering with a code of "{}".'.format(course_code))
+
+            if co_errors:
+                for co_error in co_errors:
+                    self.stderr.write(co_error)
+                raise CommandError('{} not done.'.format(verb.capitalize()))
+
+            # Remove possible duplicates (won't preserve cmd-line order)
+            course_codes = tuple(set(course_codes))
+
+        # Do the importing.  Create a separate instance of the importer for each offering, to ensure there's
+        # no state kept across offerings.
+        for course_code in course_codes:
+            importer = ImportLmsData(course_code, just_clear=options['clear'])
+            importer.process()
