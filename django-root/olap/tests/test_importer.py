e42def7f7e8509665bbc71983f9ec23aaa7214ff
Mitch Davis <mitch@alliancesoftware.com.au>
2017-09-20 14:31:16 +1000

    feat: Added two additional session calculation tests (review feedback)

--- django-root/olap/tests/test_importer.py
+++ django-root/olap/tests/test_importer.py
@@ -6,6 +6,7 @@ from django.utils.timezone import get_current_timezone
 from dashboard.tests.factories import CourseFactory
 from olap.lms_import import ImportLmsData
 from olap.models import LMSSession
+from olap.models import LMSUser
 from olap.models import PageVisit
 from olap.tests.factories import LMSUserFactory
 from olap.tests.factories import PageFactory
@@ -19,14 +20,14 @@ class ImporterSessionCalcTests(TestCase):
        For each case, construct some PageVisits with predetermined times between the visits.
        Then hand those visits to the session calculator, and check the sessions it creates.
     """
+
     def setUp(self):
         self.offering = CourseFactory()
-        self.lms_user = LMSUserFactory(course_offering=self.offering)
-        self.page = PageFactory()
+        self.test_start_datetime = datetime.datetime(2016, 7, 21, 8, 47, 21, tzinfo=get_current_timezone())
+
 
     def test_session_calculation(self):
-
-        mins_in_24h10m = 24*60+10
+        mins_in_24h10m = 24 * 60 + 10
 
         cases = (
             # PageVisit t= Inputs # Expected session starts, durations and pageviews
@@ -41,7 +42,8 @@ class ImporterSessionCalcTests(TestCase):
             ((0, mins_in_24h10m), [(0, 0, 1), (mins_in_24h10m, 0, 1)]), # Two visits 1d10m apart => two sessions
         )
 
-        start_datetime = datetime.datetime(2016, 7, 21, 8, 47, 21, tzinfo=get_current_timezone())
+        lms_user = LMSUserFactory(course_offering=self.offering)
+        page = PageFactory()
 
         importer = ImportLmsData(self.offering)
 
@@ -50,22 +52,107 @@ class ImporterSessionCalcTests(TestCase):
             PageVisit.objects.all().delete()
             LMSSession.objects.all().delete()
             # Turn tuple of time-in-minutes between views, into a tuple of visit datetimes
-            visit_datetimes = (start_datetime + datetime.timedelta(minutes=int(m)) for m in visit_event_times)
+            visit_datetimes = (self.test_start_datetime + datetime.timedelta(minutes=int(m)) for m in visit_event_times)
 
             # Create the series of visits, spaced apart by the event times
             for dt in visit_datetimes:
-                visit = PageVisitFactory(lms_user=self.lms_user, page=self.page, visited_at=dt)
+                visit = PageVisitFactory(lms_user=lms_user, page=page, visited_at=dt)
 
             # Run the session calculator
-            importer.calculate_session_for_user(self.lms_user)
+            importer.calculate_session_for_user(lms_user)
 
             # Analyse the sessions created by the calculator, and turn the start time, duration and pageviews into
             # tuples for comparison against the expected result
             actual_outputs = []
             for session in LMSSession.objects.all():
-                session_start_timedelta = session.first_visit.visited_at - start_datetime
+                session_start_timedelta = session.first_visit.visited_at - self.test_start_datetime
                 session_start_time_in_mins = int(session_start_timedelta.total_seconds() / 60)
                 actual_outputs.append((session_start_time_in_mins, session.session_length_in_mins, session.pageviews))
 
             # Now that we've assembled tuples of session starts, durations and pageviews, do they match what we expect?
             self.assertEqual(expected_outputs, actual_outputs)
+
+    def test_only_include_pages_for_a_given_user(self):
+        # Have four pages split across two users.  Make sure the calculated sessions for both users are correct,
+        # especially the aspect that visits due to one user are ignored when calculating the session for another user.
+
+        u1 = LMSUserFactory(course_offering=self.offering)
+        u1_p1 = PageFactory()
+        u1_p2 = PageFactory()
+
+        u2 = LMSUserFactory(course_offering=self.offering)
+        u2_p1 = PageFactory()
+        u2_p2 = PageFactory()
+
+        orphan = PageFactory()
+
+        # Visits for u1 which should result in a session starting at test_start_dt, lasting 25 mins
+        u1_expected_session_start_dt = self.test_start_datetime
+        v_u1_p1 = PageVisitFactory(lms_user=u1, page=u1_p1, visited_at=u1_expected_session_start_dt)
+        v_u1_p2 = PageVisitFactory(lms_user=u1, page=u1_p2, visited_at=u1_expected_session_start_dt + datetime.timedelta(minutes=25))
+        # Visits for u2 which should result in a session starting at test_start_dt+10mins, and lasting 21 mins
+        u2_expected_session_start_dt = self.test_start_datetime + datetime.timedelta(minutes=10)
+        v_u2_p1 = PageVisitFactory(lms_user=u2, page=u2_p1, visited_at=u2_expected_session_start_dt)
+        v_u2_p2 = PageVisitFactory(lms_user=u2, page=u2_p2, visited_at=u2_expected_session_start_dt + datetime.timedelta(minutes=21))
+
+        importer = ImportLmsData(self.offering)
+        importer._calculate_sessions()
+
+        sessions = LMSSession.objects.all()
+        session_info_extractor = lambda s: (s.first_visit.lms_user, s.first_visit.visited_at, s.session_length_in_mins, s.pageviews)
+        extracted_session_info = tuple(session_info_extractor(session) for session in sessions)
+
+        expected_session_info = (
+            (u1, u1_expected_session_start_dt, 25, 2),
+            (u2, u2_expected_session_start_dt, 21, 2),
+        )
+
+        self.assertEqual(expected_session_info, extracted_session_info)
+
+    def test_multiusers_same_pages(self):
+        # Have two users accessing almost the same pages.  Make sure the sessions calculated for both users include
+        #  the common pages, and the distinct ones.
+
+        u1 = LMSUserFactory(course_offering=self.offering)
+        u2 = LMSUserFactory(course_offering=self.offering)
+
+        pre_page = PageFactory()
+        common_visit_pages = PageFactory.create_batch(3)
+        post_page = PageFactory()
+
+        user_visit_info = {
+            u1.pk: (
+                (-2, pre_page),
+                (0, common_visit_pages[0]),
+                (15, common_visit_pages[1]),
+                (30, common_visit_pages[2]),
+                (39, post_page)
+            ),
+            u2.pk: (
+                (-1, pre_page),
+                (5, common_visit_pages[0]),
+                (25, common_visit_pages[1]),
+                (45, common_visit_pages[2]),
+                (89, post_page),
+            )
+        }
+
+        for user_pk, visit_list in user_visit_info.items():
+            user = LMSUser.objects.get(pk=user_pk)
+            for visit_offset_mins, page in visit_list:
+                visit_obj = PageVisitFactory(lms_user=user, page=page, visited_at=self.test_start_datetime + datetime.timedelta(minutes=visit_offset_mins))
+
+        importer = ImportLmsData(self.offering)
+        importer._calculate_sessions()
+
+        sessions = LMSSession.objects.all()
+        session_info_extractor = lambda s: (s.first_visit.lms_user, s.first_visit.visited_at, s.session_length_in_mins, s.pageviews)
+        extracted_session_info = tuple(session_info_extractor(session) for session in sessions)
+
+        expected_session_info = (
+            (u1, self.test_start_datetime + datetime.timedelta(minutes=-2), 41, 5),
+            (u2, self.test_start_datetime + datetime.timedelta(minutes=-1), 46, 4),
+            (u2, self.test_start_datetime + datetime.timedelta(minutes=89), 0, 1),
+        )
+
+        self.assertEqual(expected_session_info, extracted_session_info)
