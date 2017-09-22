095e72c428de1ca93303e0c81620f835ddc5367e
Richard Eagleton <richard@alliancesoftware.com.au>
2017-09-22 14:14:53 +1000

    feat: Apply the course access URL wrapper to the API views

--- django-root/olap/urls.py
+++ django-root/olap/urls.py
@@ -1,9 +1,13 @@
+from decorator_include import decorator_include
 from django.conf.urls import url
 
+from django_site.permissions import course_access_url_wrapper
 from olap.views import TopCourseUsersViewSet
 from olap.views import PageVisitsViewSet
 
 urlpatterns = [
-    url(r'^(?P<course_id>\d+)/course_visits/$', PageVisitsViewSet.as_view(), name='course_visits'),
-    url(r'^(?P<course_id>\d+)/top_users/$', TopCourseUsersViewSet.as_view(), name='top_users'),
+    url(r'^(?P<course_id>\d+)/', decorator_include(course_access_url_wrapper, [
+        url(r'^course_visits/$', PageVisitsViewSet.as_view(), name='course_visits'),
+        url(r'^top_users/$', TopCourseUsersViewSet.as_view(), name='top_users'),
+    ])),
 ]
--- django-root/olap/views.py
+++ django-root/olap/views.py
@@ -1,9 +1,6 @@
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
 
 from rest_framework.generics import ListAPIView
-
-from dashboard.models import CourseOffering
 
 from olap.models import LMSUser
 from olap.models import PageVisit
@@ -15,18 +12,13 @@ class PageVisitsViewSet(ListAPIView):
     serializer_class = PageVisitSerializer
 
     def get_queryset(self):
-        course_id = self.kwargs['course_id']
-        return PageVisit.objects.filter(course__id=course_id)
+        return PageVisit.objects.filter(lms_user__course_offering=self.request.course_offering)
 
 
 class TopCourseUsersViewSet(ListAPIView):
     serializer_class = TopCourseUsersSerializer
 
     def get_queryset(self):
-        course_offering = get_object_or_404(CourseOffering, pk=self.kwargs['course_id'])
-
-        # FIXME: This used to have some hardcoded start/end date filtering here (1-Jan-14 to 31-Dec-16).  Do we still need it?
-        qs = LMSUser.objects.annotate(pageviews=Count("pagevisit")).filter(course_offering=course_offering).order_by('-pageviews')[0:10]
+        qs = LMSUser.objects.annotate(pageviews=Count("pagevisit")).filter(course_offering=self.request.course_offering).order_by('-pageviews')[0:10]
 
         return qs
-
