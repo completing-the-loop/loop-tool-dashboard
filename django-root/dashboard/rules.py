029794c150972c678f14202fbd3ba52db7024ce7
Richard Eagleton <richard@alliancesoftware.com.au>
2017-09-22 12:39:39 +1000

    fix: Fix permission name

--- django-root/dashboard/rules.py
+++ django-root/dashboard/rules.py
@@ -5,4 +5,4 @@ import rules
 def is_course_offering_owner(user, course_offering):
     return user in course_offering.owners.all()
 
-rules.add_perm('dashboard.is_course_owner_offering', is_course_offering_owner)
+rules.add_perm('dashboard.is_course_offering_owner', is_course_offering_owner)
