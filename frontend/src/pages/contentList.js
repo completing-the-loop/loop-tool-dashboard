648e8f98895f96ec47d9f1761da2fc8382aaae65
Richard Eagleton <richard@alliancesoftware.com.au>
2017-09-26 12:13:56 +1000

    feat: Add the tree grid display and processing for students and events tables

--- frontend/src/pages/contentList.js
+++ frontend/src/pages/contentList.js
@@ -89,17 +89,49 @@ const init = async (
                 return _.orderBy(sortedList, ['sortOrder'], ['asc']);
             },
 
-            setSortOrder(pageViews, currentLevelPages, nextOrder, indentLevel) {
+            setSortOrder(pageList, currentLevelPages, nextOrder, indentLevel) {
                 const vue = this;
                 _.forEach(currentLevelPages, function(pageId) {
-                    pageViews[pageId].indentLevel = indentLevel;
-                    pageViews[pageId].sortOrder = nextOrder;
+                    pageList[pageId].indentLevel = indentLevel;
+                    pageList[pageId].sortOrder = nextOrder;
                     nextOrder += 1;
-                    if (pageViews[pageId].children.length) {
-                        nextOrder = vue.setSortOrder(pageViews, pageViews[pageId].children, nextOrder, indentLevel+1);
+                    if (pageList[pageId].children.length) {
+                        nextOrder = vue.setSortOrder(pageList, pageList[pageId].children, nextOrder, indentLevel+1);
                     }
                 });
                 return nextOrder;
+            },
+            processPages(apiPageList) {
+                const pageList = {};
+
+                // Set the initial state parameters for each row
+                _.forEach(apiPageList, function(page) {
+                    page.expanded = false;
+                    page.children = [];
+                    page.sortOrder = 1;
+                    page.indentLevel = 0;
+                    if (!page.parentId) {
+                        page.visible = true;
+                    } else {
+                        page.visible = false;
+                    }
+                    pageList[page.id] = page;
+                });
+
+                // Calculate the reverse relationship for parents
+                const topLevelPages = [];
+                _.forEach(pageList, function(page) {
+                    if (page.parentId) {
+                        pageList[page.parentId].children.push(page.id)
+                    } else {
+                        topLevelPages.push(page.id);
+                    }
+                });
+
+                // Recursively set the sort order and indent level for correct display
+                this.setSortOrder(pageList, topLevelPages, 1, 0);
+
+                return pageList;
             },
 
             async getPageViews() {
@@ -113,46 +145,72 @@ const init = async (
                     {id: 7, title: 'Page 3ai', parentId: 6, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
                 ];
 
-                const pageViews = {};
-
-                _.forEach(apiPageViews, function(pageView) {
-                    pageView.expanded = false;
-                    pageView.children = [];
-                    pageView.sortOrder = 1;
-                    pageView.indentLevel = 0;
-                    if (!pageView.parentId) {
-                        pageView.visible = true;
-                    } else {
-                        pageView.visible = false;
-                    }
-                    pageViews[pageView.id] = pageView;
-                });
-
-                const topLevelPages = [];
-                _.forEach(pageViews, function(pageView) {
-                    if (pageView.parentId) {
-                        pageViews[pageView.parentId].children.push(pageView.id)
-                    } else {
-                        topLevelPages.push(pageView.id);
-                    }
-                });
-
-                this.setSortOrder(pageViews, topLevelPages, 1, 0);
-
-                this.pageViews = pageViews;
+                this.pageViews = this.processPages(apiPageViews);
             },
             async getStudents() {
-                this.students = [
-                    {page_id: 1, title: 'Page 1', module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
-
+                const apiStudents = [
+                    {id: 1, title: 'Page 1', parentId: null, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
+                    {id: 2, title: 'Page 2', parentId: null, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
+                    {id: 3, title: 'Page 3', parentId: null, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
+                    {id: 4, title: 'Page 2a', parentId: 2, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
+                    {id: 5, title: 'Page 2b', parentId: 2, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
+                    {id: 6, title: 'Page 3a', parentId: 3, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
+                    {id: 7, title: 'Page 3ai', parentId: 6, module: 'module', weeks: {1: 0, 2: 1, 3: 6, 4: 7, 5: 3, 6: 8, 7:4, 8:22, 9: 11, 10:12, 11: 33, 12: 7, 13: 4, 14: 7}, total: 72, percent: 91},
                 ];
+
+                this.students = this.processPages(apiStudents);
             },
             async getEvents() {
-                this.events = [
+                const apiEvents = [
                     {
-                        page_id: 1,
+                        id: 1,
                         title: 'Page 1',
                         module: 'module',
+                        parentId: null,
+                        weeks: {
+                            1: {before: 0, after: 4},
+                            2: {before: 0, after: 4},
+                            3: {before: 0, after: 4},
+                            4: {before: 0, after: 4},
+                            5: {before: 0, after: 4},
+                            6: {before: 0, after: 4},
+                            7: {before: 0, after: 4},
+                            8: {before: 0, after: 4},
+                            9: {before: 0, after: 4},
+                            10: {before: 0, after: 4},
+                            11: {before: 0, after: 4},
+                            12: {before: 0, after: 4},
+                            13: {before: 0, after: 4},
+                            14: {before: 0, after: 4}
+                        }
+                    },
+                    {
+                        id: 2,
+                        title: 'Page 2',
+                        module: 'module',
+                        parentId: null,
+                        weeks: {
+                            1: {before: 0, after: 4},
+                            2: {before: 0, after: 4},
+                            3: {before: 0, after: 4},
+                            4: {before: 0, after: 4},
+                            5: {before: 0, after: 4},
+                            6: {before: 0, after: 4},
+                            7: {before: 0, after: 4},
+                            8: {before: 0, after: 4},
+                            9: {before: 0, after: 4},
+                            10: {before: 0, after: 4},
+                            11: {before: 0, after: 4},
+                            12: {before: 0, after: 4},
+                            13: {before: 0, after: 4},
+                            14: {before: 0, after: 4}
+                        }
+                    },
+                    {
+                        id: 3,
+                        title: 'Page 1a',
+                        module: 'module',
+                        parentId: 1,
                         weeks: {
                             1: {before: 0, after: 4},
                             2: {before: 0, after: 4},
@@ -171,6 +229,7 @@ const init = async (
                         }
                     },
                 ];
+                this.events = this.processPages(apiEvents);
             },
         },
         watch: {
