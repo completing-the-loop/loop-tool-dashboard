7c3783061a79c08c9d8a01747d4825cf6b3defde
Richard Eagleton <richard@alliancesoftware.com.au>
2017-10-03 16:08:49 +1100

    feat: Add task to globally process data files and prepare for import

--- django-root/olap/management/commands/import_olap_data.py
+++ django-root/olap/management/commands/import_olap_data.py
@@ -5,8 +5,9 @@ from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
 
 from dashboard.models import CourseOffering
+from olap.tasks import import_olap_task
+from olap.utils import get_course_export_data
 
-from olap.lms_import import ImportLmsData
 
 class Command(BaseCommand):
     help = "Import LMS data for one or more course offerings"
@@ -36,53 +37,59 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        course_codes = options['offering_code']
+        course_codes = []
+        requested_course_codes = options['offering_code']
         verb = 'clear' if options['clear'] else 'import'
+        course_export_data = get_course_export_data()
+        all_course_codes = course_export_data['courses'].keys()
 
         if options['all']:
-            if len(course_codes) > 0:
+            if len(requested_course_codes) > 0:
                 raise CommandError('Course offering codes cannot be specified at the same time as --all.')
-            if len(CourseOffering.objects.all()) == 0:
-                raise CommandError('Cannot {} because there are no course offerings in the database.'.format(verb))
-            course_offerings = CourseOffering.objects.all()
+            course_codes = all_course_codes
         else:
-            if len(course_codes) == 0:
+            if len(requested_course_codes) == 0:
                 raise CommandError('Please supply the code of the course offering to {}.'.format(verb))
+
             error_msgs = []
-            course_offerings = []
-            for course_code in course_codes:
-                try:
-                    course_offerings.append(CourseOffering.objects.get(code=course_code))
-                except CourseOffering.DoesNotExist:
-                    error_msgs.append('There is no course offering with a code of "{}".'.format(course_code))
+            for course_code in requested_course_codes:
+                if course_code in all_course_codes:
+                    course_codes.append(course_code)
+                else:
+                    error_msgs.append('There is no course offering able to be imported with a code of "{}".'.format(course_code))
 
             if error_msgs:
                 for error_msg in error_msgs:
                     self.stderr.write(error_msg)
                 raise CommandError('{} not done.'.format(verb.capitalize()))
 
+        if len(course_codes) == 0:
+            raise CommandError('Cannot {} because there are no course offerings able to be imported.'.format(verb))
+
         # Do the importing.  Create a separate instance of the importer for each offering, to ensure there's
         # no state kept across offerings.
         error_count = 0
-        for course_offering in course_offerings:
+        for course_code in course_codes:
             try:
-                importer = ImportLmsData(course_offering, just_clear=options['clear'])
-                importer.process()
+                course_offering = CourseOffering.objects.get(code=course_code)
+                course_offering.is_importing = True
+                course_offering.save()
+                import_olap_task.delay(course_offering.id, course_export_data['courses'][course_code]['filename'])
             except:
                 error_count += 1
-                self.stderr.write('An error occurred when {}ing the data for {}:'.format(verb, course_offering.code))
+                self.stderr.write('An error occurred when {}ing the data for {}:'.format(verb, course_code))
                 if not options['trace']:
                     self.stdout.write('Run again with --trace to show a diagnostic trace.')
                 else:
                     exc_info = sys.exc_info()
                     traceback.print_exception(*exc_info)
 
-        plural_offerings = len(course_offerings) != 1
+        plural_offerings = len(course_codes) != 1
         plural_errors = error_count != 1
         if error_count == 0:
             if plural_offerings:
-                self.stdout.write('All {} {}s were successful.'.format(len(course_offerings), verb))
+                self.stdout.write('All {} {}s were successful.'.format(len(course_codes), verb))
             else:
                 self.stdout.write('The {} was successful.'.format(verb))
         else:
-            self.stderr.write('{} {}{} out of {} {} unsuccessful.'.format(error_count, verb, 's' if plural_errors else '', len(course_offerings), 'were' if plural_errors else 'was'))
+            self.stderr.write('{} {}{} out of {} {} unsuccessful.'.format(error_count, verb, 's' if plural_errors else '', len(course_codes), 'were' if plural_errors else 'was'))
--- django-root/olap/tasks.py
+++ django-root/olap/tasks.py
@@ -1,8 +1,52 @@
-from django.core.management import call_command
+from django.conf import settings
+from unipath.path import Path
 
+from dashboard.models import CourseOffering
 from django_site.celery import app
+from olap.lms_import import ImportLmsData
+from olap.models import PageVisit
+from olap.models import SubmissionAttempt
+from olap.models import SummaryPost
+from olap.utils import get_course_export_data
 
 
 @app.task(bind=True)
-def import_olap_task(self):
-    call_command('import_olap_data')
+def import_olap_task(self, course_id, filename):
+    course_offering = CourseOffering.objects.get(id=course_id)
+    file_path = Path.join(settings.DATA_PROCESSING_DIR, filename)
+
+    try:
+        importer = ImportLmsData(course_offering, file_path)
+        importer.process()
+
+        latest_activity = [
+            PageVisit.objects.filter(page__course_offering=course_offering).latest('visited_at').visited_at,
+            SubmissionAttempt.objects.filter(page__course_offering=course_offering).latest('attempted_at').attempted_at,
+            SummaryPost.objects.filter(course_offering=course_offering).latest('posted_at').posted_at,
+        ]
+        course_offering.last_activity_at = max(latest_activity)
+    finally:
+        course_offering.is_importing = False
+        course_offering.save()
+
+
+@app.task(bind=True)
+def process_data_exports(self):
+    processing_data_folder = settings.DATA_PROCESSING_DIR
+    export_data = get_course_export_data()
+
+    export_data_mapping = { course_offering['filename']: course_offering['id'] for course_offering in export_data['courses'] }
+
+    import_files = Path(export_data['import_location']).listdir()
+
+    for import_file in import_files:
+        try:
+            course_offering = CourseOffering.objects.get(id=export_data_mapping[import_file.name])
+            import_file.move(processing_data_folder)
+            course_offering.is_importing = True
+            course_offering.save()
+
+            import_olap_task.delay(course_offering.id, import_file.name)
+        except KeyError:
+            import_file.remove()
+
