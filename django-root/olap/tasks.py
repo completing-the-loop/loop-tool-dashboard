0f9fb0c7c15e2d6f14d7f66461bc21bc9bfc9514
Richard Eagleton <richard@alliancesoftware.com.au>
2017-10-06 14:55:42 +1100

    feat: Initial refactor of Blackboard importer to use CSV

--- django-root/olap/tasks.py
+++ django-root/olap/tasks.py
@@ -4,40 +4,17 @@ from unipath.path import Path
 from dashboard.models import CourseOffering
 from django_site.celery import app
 from olap.lms_import import ImportLmsData
-from olap.models import PageVisit
-from olap.models import SubmissionAttempt
-from olap.models import SummaryPost
 from olap.utils import get_course_import_metadata
 
 
 @app.task(bind=True)
 def import_olap_task(self, course_id, filename, just_clear=False):
-    course_offering = CourseOffering.objects.get(id=course_id, just_clear=just_clear)
-    file_path = Path.join(settings.DATA_PROCESSING_DIR, filename)
+    course_offering = CourseOffering.objects.get(id=course_id)
+    file_path = Path(settings.DATA_PROCESSING_DIR, filename)
 
     try:
-        importer = ImportLmsData(course_offering, file_path)
+        importer = ImportLmsData(course_offering, file_path, just_clear=just_clear)
         importer.process()
-
-        latest_activity = []
-
-        try:
-            latest_activity.append(PageVisit.objects.filter(page__course_offering=course_offering).latest('visited_at').visited_at)
-        except PageVisit.DoesNotExist:
-            pass
-
-        try:
-            latest_activity.append(SubmissionAttempt.objects.filter(page__course_offering=course_offering).latest('attempted_at').attempted_at)
-        except SubmissionAttempt.DoesNotExist:
-            pass
-
-        try:
-            latest_activity.append(SummaryPost.objects.filter(course_offering=course_offering).latest('posted_at').posted_at)
-        except SummaryPost.DoesNotExist:
-            pass
-
-        if len(latest_activity):
-            course_offering.last_activity_at = max(latest_activity)
     finally:
         course_offering.is_importing = False
         course_offering.save()
