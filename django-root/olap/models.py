2844b96faa2057727ab7217b14c44b269942740d
Mitch Davis <mitch@alliancesoftware.com.au>
2017-10-19 18:56:14 +1100

    feat: Added method to return full name.

--- django-root/olap/models.py
+++ django-root/olap/models.py
@@ -75,11 +75,14 @@ class LMSUser(models.Model):
         unique_together = (('course_offering', 'lms_user_id'), )
 
     def __str__(self):
-        possible_name = " ".join((self.firstname, self.lastname))
-        if possible_name:
-            return "<LMSUser {}: {}>".format(self.lms_user_id, possible_name)
+        name = self.full_name()
+        if name:
+            return "<LMSUser {}: {}>".format(self.lms_user_id, name)
         else:
             return "<LMSUser {}: ## Phantom user ##>".format(self.lms_user_id)
+
+    def full_name(self):
+        return " ".join((self.firstname, self.lastname))
 
 # CREATE TABLE `fact_coursevisits` (
 #   `id` int(11) NOT NULL,
