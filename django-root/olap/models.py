212cfa7262f1b09171caf8159e480413404cf460
Mitch Davis <mitch@alliancesoftware.com.au>
2017-10-19 18:53:45 +1100

    fix: Really really change SubmissionAttempt.grade to DecimalField

--- django-root/olap/models.py
+++ django-root/olap/models.py
@@ -216,7 +216,7 @@ class SubmissionAttempt(models.Model):
     attempted_at = models.DateTimeField()
     page = models.ForeignKey(Page) # Was called content_id
     lms_user = models.ForeignKey(LMSUser)
-    grade = models.CharField(max_length=50)
+    grade = models.DecimalField(decimal_places=4, max_digits=7) # Up to 999.9999
 
     class Meta:
         unique_together = (('lms_user', 'page', 'attempted_at'),)
