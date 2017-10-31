9344abbf474cfaaec5384b19831d442023cee307
Richard Eagleton <richard@alliancesoftware.com.au>
2017-10-31 14:00:00 +1100

    refactor: Remove async/await from Vue component

--- frontend/src/components/range_graph.vue
+++ frontend/src/components/range_graph.vue
@@ -30,175 +30,172 @@
                 type: String,
             },
         },
-    data: function() {
-        return {
-            coursePageVisits: [],
-        };
-    },
-    mounted: async function mounted() {
-
+    mounted() {
         let params = {};
         if (this.studentId) {
             params['student_id'] = this.studentId;
         }
 
-        this.coursePageVisits = await get(`${this.courseId}/course_page_visits`, params);
+        const vue = this;
 
-        let maxVisits = 0;
-        const dates = [];
-        const contentVisits = [];
-        const communicationVisits = [];
-        const assessmentVisits = [];
-        const singleEventsData = [];
-        const submissionEventsData = [];
-        const singleEventsText = [];
-        const submissionEventsText = [];
+        get(`${this.courseId}/course_page_visits`, params).then((coursePageVisits) => {
 
-        // Initial loop through data for page views
-        _.forEach(this.coursePageVisits, function(visit) {
-            dates.push(visit.day);
-            contentVisits.push(visit.contentVisits);
-            communicationVisits.push(visit.communicationVisits);
-            assessmentVisits.push(visit.assessmentVisits);
-            if (visit.contentVisits > maxVisits) {
-                maxVisits = visit.contentVisits;
-            }
-            if (visit.communicationVisits > maxVisits) {
-                maxVisits = visit.communicationVisits;
-            }
-            if (visit.assessmentVisits > maxVisits) {
-                maxVisits = visit.assessmentVisits;
-            }
-        });
+            let maxVisits = 0;
+            const dates = [];
+            const contentVisits = [];
+            const communicationVisits = [];
+            const assessmentVisits = [];
+            const singleEventsData = [];
+            const submissionEventsData = [];
+            const singleEventsText = [];
+            const submissionEventsText = [];
 
-        // Second loop to build events data
-        _.forEach(this.coursePageVisits, function(visit) {
-            if (visit.singleEvents.length) {
-                singleEventsData.push(maxVisits+1);
-                singleEventsText.push(_.join(visit.singleEvents, ', '));
-            } else {
-                singleEventsData.push(null);
-                singleEventsText.push(null);
-            }
-            if (visit.submissionEvents.length) {
-                submissionEventsData.push(maxVisits+1);
-                submissionEventsText.push(_.join(visit.submissionEvents, ', '));
-            } else {
-                submissionEventsData.push(null);
-                submissionEventsText.push(null);
-            }
-        });
+            // Initial loop through data for page views
+            _.forEach(coursePageVisits, function(visit) {
+                dates.push(visit.day);
+                contentVisits.push(visit.contentVisits);
+                communicationVisits.push(visit.communicationVisits);
+                assessmentVisits.push(visit.assessmentVisits);
+                if (visit.contentVisits > maxVisits) {
+                    maxVisits = visit.contentVisits;
+                }
+                if (visit.communicationVisits > maxVisits) {
+                    maxVisits = visit.communicationVisits;
+                }
+                if (visit.assessmentVisits > maxVisits) {
+                    maxVisits = visit.assessmentVisits;
+                }
+            });
 
-        const graphData = [
-            {
-                type: "scatter",
-                mode: "lines",
-                name: "Content",
-                x: dates,
-                y: contentVisits,
-            },
-            {
-                type: "scatter",
-                mode: "lines",
-                name: "Communication",
-                x: dates,
-                y: communicationVisits,
-            },
-            {
-                type: "scatter",
-                mode: "lines",
-                name: "Assessment",
-                x: dates,
-                y: assessmentVisits,
-            },
-            {
-                type: "scatter",
-                mode: "markers+text",
-                name: "Single Events",
-                x: dates,
-                y: singleEventsData,
-                text: singleEventsText,
-                textposition: "top center",
-            },
-            {
-                type: "scatter",
-                mode: "markers+text",
-                name: "Submission Events",
-                x: dates,
-                y: submissionEventsData,
-                text: submissionEventsText,
-                textposition: "top center",
-            },
-        ];
+            // Second loop to build events data
+            _.forEach(coursePageVisits, function(visit) {
+                if (visit.singleEvents.length) {
+                    singleEventsData.push(maxVisits+1);
+                    singleEventsText.push(_.join(visit.singleEvents, ', '));
+                } else {
+                    singleEventsData.push(null);
+                    singleEventsText.push(null);
+                }
+                if (visit.submissionEvents.length) {
+                    submissionEventsData.push(maxVisits+1);
+                    submissionEventsText.push(_.join(visit.submissionEvents, ', '));
+                } else {
+                    submissionEventsData.push(null);
+                    submissionEventsText.push(null);
+                }
+            });
 
-        // Make the range slider range a bit longer to show shaded sidebars
-        const rangeStart = moment(this.courseStart).add(-2, 'weeks').format('YYYY-MM-DD');
-        const rangeEnd = moment(this.courseStart).add(this.numWeeks + 2, 'weeks').format('YYYY-MM-DD');
-
-        const graphLayout = {
-            xaxis: {
-                rangeselector: {
-                    buttons: [
-                        {
-                            count: 1,
-                            label: '1m',
-                            step: 'month',
-                            stepmode: 'backward'
-                        },
-                        {
-                            count: 3,
-                            label: '3m',
-                            step: 'month',
-                            stepmode: 'backward'
-                        },
-                        {
-                            count: 6,
-                            label: '6m',
-                            step: 'month',
-                            stepmode: 'backward'
-                        },
-                        {
-                            count: 1,
-                            label: 'YTD',
-                            step: 'year',
-                            stepmode: 'todate'
-                        },
-                        {
-                            count: 1,
-                            label: '1y',
-                            step: 'year',
-                            stepmode: 'backward'
-                        },
-                        {
-                            label: 'All',
-                            step: 'all',
-                        },
-                    ],
-                    y: 1.1,
+            const graphData = [
+                {
+                    type: "scatter",
+                    mode: "lines",
+                    name: "Content",
+                    x: dates,
+                    y: contentVisits,
                 },
-                rangeslider: {
-                    thickness: 0.3,
-                    range: [rangeStart, rangeEnd],
+                {
+                    type: "scatter",
+                    mode: "lines",
+                    name: "Communication",
+                    x: dates,
+                    y: communicationVisits,
                 },
-                type: 'date',
-            },
-            yaxis: {
-                range: [0, maxVisits + 2],
-            }
-        };
+                {
+                    type: "scatter",
+                    mode: "lines",
+                    name: "Assessment",
+                    x: dates,
+                    y: assessmentVisits,
+                },
+                {
+                    type: "scatter",
+                    mode: "markers+text",
+                    name: "Single Events",
+                    x: dates,
+                    y: singleEventsData,
+                    text: singleEventsText,
+                    textposition: "top center",
+                },
+                {
+                    type: "scatter",
+                    mode: "markers+text",
+                    name: "Submission Events",
+                    x: dates,
+                    y: submissionEventsData,
+                    text: submissionEventsText,
+                    textposition: "top center",
+                },
+            ];
 
-        const graphConfig = {
-            modeBarButtonsToRemove: [
-                'sendDataToCloud',
-            ],
-        };
+            // Make the range slider range a bit longer to show shaded sidebars
+            const rangeStart = moment(vue.courseStart).add(-2, 'weeks').format('YYYY-MM-DD');
+            const rangeEnd = moment(vue.courseStart).add(vue.numWeeks + 2, 'weeks').format('YYYY-MM-DD');
 
-        Plotly.newPlot(
-            this.graphId,
-            graphData,
-            graphLayout,
-            graphConfig,
-        );
+            const graphLayout = {
+                xaxis: {
+                    rangeselector: {
+                        buttons: [
+                            {
+                                count: 1,
+                                label: '1m',
+                                step: 'month',
+                                stepmode: 'backward'
+                            },
+                            {
+                                count: 3,
+                                label: '3m',
+                                step: 'month',
+                                stepmode: 'backward'
+                            },
+                            {
+                                count: 6,
+                                label: '6m',
+                                step: 'month',
+                                stepmode: 'backward'
+                            },
+                            {
+                                count: 1,
+                                label: 'YTD',
+                                step: 'year',
+                                stepmode: 'todate'
+                            },
+                            {
+                                count: 1,
+                                label: '1y',
+                                step: 'year',
+                                stepmode: 'backward'
+                            },
+                            {
+                                label: 'All',
+                                step: 'all',
+                            },
+                        ],
+                        y: 1.1,
+                    },
+                    rangeslider: {
+                        thickness: 0.3,
+                        range: [rangeStart, rangeEnd],
+                    },
+                    type: 'date',
+                },
+                yaxis: {
+                    range: [0, maxVisits + 2],
+                }
+            };
+
+            const graphConfig = {
+                modeBarButtonsToRemove: [
+                    'sendDataToCloud',
+                ],
+            };
+
+            Plotly.newPlot(
+                vue.graphId,
+                graphData,
+                graphLayout,
+                graphConfig,
+            );
+        });
 
     },
   };
