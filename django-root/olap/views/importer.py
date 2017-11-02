61e51796760dbe84c5cc6f6597ed9da7ca62481b
Richard Eagleton <richard@alliancesoftware.com.au>
2017-11-03 09:47:45 +1100

    feat: Update the importer and import metadata endpoint to work with multiple servers

--- django-root/olap/views/importer.py
+++ django-root/olap/views/importer.py
@@ -1,7 +1,9 @@
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from dashboard.models import LMSServer
 from olap.utils import get_course_import_metadata
 
 
@@ -9,5 +11,9 @@ class CourseImportsApiView(APIView):
     authentication_classes = (TokenAuthentication,)
 
     def get(self, request, format=None):
-        api_data = get_course_import_metadata()
-        return Response(api_data)
+        try:
+            lms_server = LMSServer.objects.get(token=request.auth)
+            api_data = get_course_import_metadata(lms_server)
+            return Response(api_data)
+        except LMSServer.DoesNotExist:
+            raise NotFound()
